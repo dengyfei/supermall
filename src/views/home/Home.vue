dbb5b1e2d72b0b5780d8d0e894ee9e982d25f26f
Deng YF <1240517822@qq.com>
2021-02-07 10:22:57 +0800

    首页导航

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,3 +1,24 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物车</div>
+    </nav-bar>
+  </div>
 </template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
