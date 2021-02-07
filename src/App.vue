58a524370c908b27447cb140109be4a76c88a94e
Deng YF <1240517822@qq.com>
2021-02-07 09:52:17 +0800

    tabBar的导入

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
-
     <router-view/>
+    <main-tab-bar/>
   </div>
 </template>
+
+<script>
+import MainTabBar from 'components/content/MainTabBar'
+
+export default {
+  components: {
+    MainTabBar
+  }
+}
+</script>
 
 <style>
   @import 'assets/css/base.css'
--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,27 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: ()=> import('../views/home/Home.vue')
+  },
+  {
+    path: '/category',
+    component: ()=> import('../views/category/Category.vue')
+  },
+  {
+    path: '/shopcart',
+    component: ()=> import('../views/shopcart/Shopcart.vue')
+  },
+  {
+    path: '/profile',
+    component: ()=> import('../views/profile/Profile.vue')
+  }
+  
 ]
 
 const router = new VueRouter({
